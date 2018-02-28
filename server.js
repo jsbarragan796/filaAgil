2538e7dd3a9237f661e3638a8f618ffc8f6441b0
juanSebastian <js.barragan796@uniandes.edu.co>
2018-02-27 21:46:35 -0500

    inicial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+
+const MongoClient = require("mongodb").MongoClient;
+
+// Connection URL
+const url = process.env.MLAB;
+var port = process.env.PORT || 3000;
+
+// function findDocuments(db, callback) {
+//   const collection = db.collection("tweets");
+//   collection.find({}).toArray((err, docs) => {
+//     if (err) throw err;
+//     console.log("inicio ");
+//     callback(docs);
+//   });
+// }
+//
+// function findDocumentsParam(db, callback,search) {
+//   const collection = db.collection("tweets");
+//   console.log("busqueda search!");
+//   var query ={
+//     $or:[
+//       {"user.screen_name":{$regex : search, $options : "i"}},
+//       {text:{$regex : search, $options : "i"}}
+//     ]
+//   };
+//   collection.find(query).toArray((err, docs) => {
+//     if (err) throw err;
+//     console.log("busqueda search!!!");
+//     callback(docs);
+//   });
+// }
+
+
+function getSomeThing () {
+  MongoClient.connect(url, (err, client) => {
+    if (err) throw err;
+    console.log("Connected");
+  //  const db = client.db("webdev_tweets");
+  }
+  //  client.close();
+  );
+}
+
+app.use(express.static("public"));
+app.get("/index", (req, res) => {
+  getSomeThing();
+});
+
+app.listen(port, () => console.log("Example app listening on port"));
