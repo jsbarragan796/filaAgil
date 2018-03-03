ba3b86747f50205bd0e84aa237d096b9efd0d486
cawolf <camilozambranovotto@hotmail.com>
2018-03-03 16:21:10 -0500

    REST e ingredientes
    ya se envian los :meat_on_bone: y se corrigio el gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+"use strict";
 require("dotenv").config();
 var express = require("express");
 const MongoClient = require("mongodb").MongoClient;
@@ -7,7 +8,7 @@ var router = new express.Router();
 // Connection URL
 const url = process.env.MLAB;
 
-function find (db, callback) {
+function find(db, callback) {
   const collection = db.collection("page_photos");
   collection.find({}).toArray((err, docs) => {
     assert.equal(err, null);
@@ -16,7 +17,7 @@ function find (db, callback) {
   });
 }
 
-function getPhotos (callback) {
+function getPhotos(callback) {
   MongoClient.connect(url, (err, client) => {
     assert.equal(err, null);
     console.log("Connected");
@@ -26,10 +27,36 @@ function getPhotos (callback) {
   });
 }
 
+function getIngredients(callback) {
+  // conexion a la base de datos
+  MongoClient.connect(url)
+    .then((client) => { //una vez conectado
+      const db = client.db("filas_agiles"); //pide la base de datos
+      const collection = db.collection("ingredientes"); //pide la collecion
+      collection.find({}).toArray() //pide todos los ingredientes y cuando los convierte a una lista
+        .then(callback)//responde al cliente con ellos
+        .catch((excep) => {
+          console.log("Hubo un error en array:"); //si hay un problema con pasar los datos a arreglo 
+          console.log(excep.message); //lo despliega
+        });
+      client.close(); //cierra la conexion
+    })
+    .catch((excep) => {
+      console.log("Hubo un error en conexion:");  //si hay un problema con la conexion
+      console.log(excep.message); //lo despliega
+    });
+}
+
 /* GET home page. */
 router.get("/images", (req, res) => {
   console.log("inicio");
   getPhotos((pagePhotos) => res.send(pagePhotos));
+});
+
+// GET ingredientes de Mongo
+router.get("/ingredientes", (req, res) => {
+  console.log("se van a entregar ingredientes");
+  getIngredients((datos)=>res.send(datos));
 });
 
 module.exports = router;
